8af5a362552a0d7ce129e647026d533fde446241
MachineGunnur <45913297+MachineGunnur@users.noreply.github.com>
2019-04-26 17:18:34 +0000

    increase vote limit per ip address

--- web/index.php
+++ web/index.php
@@ -1286,7 +1286,7 @@ $app->group('/api/public', function () use ($app) {
             $checkVoteCount = $dbh->prepare("SELECT COUNT(*) FROM `votes` WHERE `uid` = INET6_ATON(:ip)");
             $checkVoteCount->bindParam(':ip', $ipAddress, \PDO::PARAM_STR);
             $checkVoteCount->execute();
-            if ($checkVoteCount->fetchColumn() >= 3) {
+            if ($checkVoteCount->fetchColumn() >= 100) {
                 return $response->withJson(['SUCCESS' => false, 'MSG' => _('Your vote limit has exceeded.')]);
             }
 
