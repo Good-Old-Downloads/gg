51c5313cc94ffa3d5aa9fb4847a8c8f8e70cf5e5
MachineGunnur <45913297+MachineGunnur@users.noreply.github.com>
2019-07-12 18:46:08 +0000

    up vote count (TEMP!)

--- web/index.php
+++ web/index.php
@@ -1326,7 +1326,7 @@ $app->group('/api/public', function () use ($app) {
             $checkVoteCount = $dbh->prepare("SELECT COUNT(*) FROM `votes` WHERE `uid` = INET6_ATON(:ip)");
             $checkVoteCount->bindParam(':ip', $ipAddress, \PDO::PARAM_STR);
             $checkVoteCount->execute();
-            if ($checkVoteCount->fetchColumn() >= 10) {
+            if ($checkVoteCount->fetchColumn() >= 9999) {
                 return $response->withJson(['SUCCESS' => false, 'MSG' => _('Your vote limit has exceeded.')]);
             }
 
